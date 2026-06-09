399c26546b6347bd1d3e049f0bafde1e43fc9c09
Anil chaturvedi <74783239+chaturvedi-anil@users.noreply.github.com>
2026-06-09 12:42:31 +0530

    feat: add get all users endpoint

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -2,12 +2,19 @@ import { Router } from "express";
 import { login, me, register } from "../contollers/auth.controller.js";
 import validate from "../middlewares/validation.middleware.js";
 import { isAuthenticated } from "../middlewares/auth.middleware.js";
-import { loginSchema, registerSchema } from "../validations/auth.validation.js";
+import { authorize } from "../middlewares/role.middleware.js";
+import {
+  loginSchema,
+  registerSchema,
+  getAllUsers,
+  getMe,
+} from "../validations/auth.validation.js";
 
 const authRouter = Router();
 
 authRouter.post("/register", validate(registerSchema), register);
 authRouter.post("/login", validate(loginSchema), login);
 
-authRouter.get("/me", isAuthenticated, me);
+authRouter.get("/me", isAuthenticated, getMe);
+authRouter.get("/users", isAuthenticated, authorize("admin"), getAllUsers);
 export default authRouter;
